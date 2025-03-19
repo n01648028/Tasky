8ac7ed5ac565ca8224beca3cf1da2815311ffd47
n01648028 <146011951+n01648028@users.noreply.github.com>
2025-03-19 18:34:22 -0400

    update

--- upload.sh
+++ upload.sh
@@ -1,8 +1,8 @@
 ac_i=abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_
-ac_temp=$(printf "%s" $ac_i | cut -c 7)ithub_pat_11BCZ7OLY03Kw3gy9l3F3N_3Lnet5nKQhVO4UHUmQFaNspS5VRUDmwp3z2YT826YCP4U5CY7LJnemUNvxu
+ac_temp=$(printf "%s" $ac_i | cut -c 7)ithub_pat_11BCZ7OLY09kIcaqniTNKu_yU9MppfkeXnXJUBeT7MSect4rVdUtITmppjvmfcGRRtVL7MMGGBNfJeyU4o
 #echo $ac_temp
 git pull
 git add .
 git commit -m update
-git push https://$ac_temp@github.com/n01648028/calendar-scheduler
+git push https://$ac_temp@github.com/n01648028/Tasky
 git pull
