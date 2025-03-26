8a706cf650497d736dfed4d02d2250ab971a117f
n01648028 <146011951+n01648028@users.noreply.github.com>
2025-03-26 19:23:07 -0400

    update

--- Tasky/src/main/java/com/humber/Tasky/TaskyApplication.java
+++ Tasky/src/main/java/com/humber/Tasky/TaskyApplication.java
@@ -1,7 +1,10 @@
 package com.humber.Tasky;
 
+import com.humber.Tasky.controllers.AuthController;
 import com.humber.Tasky.models.Dish;
+import com.humber.Tasky.models.MyUser;
 import com.humber.Tasky.services.DishService;
+import com.humber.Tasky.services.UserService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,10 +14,12 @@ public class TaskyApplication implements CommandLineRunner {
 
 // constructor injection - Dish Service
 	private final DishService dishService;
+	private final UserService userService;
 
-	public TaskyApplication(DishService dishService) {
+	public TaskyApplication(DishService dishService, UserService userService) {
 		this.dishService = dishService;
-	}
+        this.userService = userService;
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(TaskyApplication.class, args);
@@ -22,6 +27,8 @@ public class TaskyApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+		MyUser admin = new MyUser(0, "templates/admin", "password", "ADMIN");
+		userService.saveUser(admin);
 		dishService.saveDish(new Dish("Pizza", "Italian", 10.0));
 	}
 }
